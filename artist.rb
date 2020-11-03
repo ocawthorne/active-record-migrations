eb1d85c330ec820c32bafbe38e1ef375604a5026
ocawthorne <ocawthorne@gmail.com>
2020-11-03 18:51:25 +0000

    active-record-migrations-031120

--- artist.rb
+++ artist.rb
@@ -1,6 +1,10 @@
 class Artist < ActiveRecord::Base
     def change
         create_table :artists do |t|
+            t.string :name
+            t.string :genre
+            t.integer :age
+            t.string :hometown
         end
     end
 end
